103e2b0c311a856875eced1f3935274de6f68d0a
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-02-26 22:33:43 -0500

    feat: Agregar encapsulamiento a clase Asignatura

--- src/main/java/org/example/Asignatura.java
+++ src/main/java/org/example/Asignatura.java
@@ -1,9 +1,51 @@
 package org.example;
 
+
 public class Asignatura {
          String codigo;
          String nombre;
          int credito;
          String docente;
 
+    public Asignatura() {
+    }
+
+    public Asignatura(String codigo, String nombre, int credito, String docente) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.credito = credito;
+        this.docente = docente;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
 }
