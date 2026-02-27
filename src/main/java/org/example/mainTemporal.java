54a89595726aaeedc403fb78a4d835b91c0799ec
Mateo Calderon <mcalderona@unicesar.edu.co>
2026-02-27 00:31:01 -0500

    feat: implementar READ - listar y buscar asignatura

--- src/main/java/org/example/mainTemporal.java
+++ src/main/java/org/example/mainTemporal.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class mainTemporal {
      String codigo;
      String nombre;
      String apellido;
      int edad;
      int semestre;
+
+    static ArrayList<Asignatura> lista = new ArrayList<>();
 
     public static void registrarAsignatura() {
 
@@ -21,9 +25,45 @@ public class mainTemporal {
         System.out.print("Ingrese nombre: ");
         String nombre = sc.nextLine();
 
+        lista.add(new Asignatura(codigo, nombre));
+
         System.out.println("\nAsignatura registrada:");
         System.out.println("Codigo: " + codigo);
         System.out.println("Nombre: " + nombre);
+    }
+
+
+    public static void listarAsignaturas() {
+
+        if (listaAsignaturas.isEmpty()) {
+            System.out.println("No hay asignaturas registradas.");
+            return;
+        }
+
+        for (Asignatura a : listaAsignaturas) {
+            System.out.println("Codigo: " + a.getCodigo());
+            System.out.println("Nombre: " + a.getNombre());
+        }
+    }
+
+    public static void buscarAsignatura() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese codigo a buscar: ");
+        String codigoBuscado = sc.nextLine();
+
+        for (Asignatura a : listaAsignaturas) {
+            if (a.getCodigo().equalsIgnoreCase(codigoBuscado)) {
+
+                System.out.println("Asignatura encontrada:");
+                System.out.println("Codigo: " + a.getCodigo());
+                System.out.println("Nombre: " + a.getNombre());
+                return;
+            }
+        }
+
+        System.out.println("Asignatura no encontrada.");
     }
 
 
