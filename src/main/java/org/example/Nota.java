70e9977fca37300e625c992d1cf313bdbea9309b
Alberto Mario <albertommendoza@unicesar.edu.co>
2026-02-26 22:52:13 -0500

    feat: Agregar encapsulamiento a clase Nota

--- src/main/java/org/example/Nota.java
+++ src/main/java/org/example/Nota.java
@@ -5,5 +5,47 @@ public class Nota {
      Asignatura asignatura;
      Double valor;
      String periodo;
-     
+
+    public Nota() {
+    }
+
+    public Nota(Estudiante estudiante, Asignatura asignatura, Double valor, String periodo) {
+        this.estudiante = estudiante;
+        this.asignatura = asignatura;
+        this.valor = valor;
+        this.periodo = periodo;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Asignatura getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+    
 }
